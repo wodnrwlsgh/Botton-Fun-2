e85608edd3741dfe6364b5b8db535cc127dc39a2
D7703_29 <d7703_29@d7703-29ui-imac.local>
2018-03-12 15:33:18 +0900

    Initial Commit

---
+++ Botton Fun 2UITests/Botton_Fun_2UITests.swift
@@ -0,0 +1,36 @@
+//
+//  Botton_Fun_2UITests.swift
+//  Botton Fun 2UITests
+//
+//  Created by D7703_29 on 2018. 3. 12..
+//  Copyright © 2018년 정재욱. All rights reserved.
+//
+
+import XCTest
+
+class Botton_Fun_2UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
